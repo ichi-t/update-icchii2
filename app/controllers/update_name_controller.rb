c626fbd89bdfb192ccdbe639e3b6f9622932416c
kurosanex <kurosanex5010@me.com>
2015-09-01 00:09:58 +0900

    [add] controller

--- app/controllers/update_name_controller.rb
+++ app/controllers/update_name_controller.rb
@@ -1,2 +1,7 @@
 class UpdateNameController < ApplicationController
+  def index
+    update = params.permit(:cpu, :mem,:server_id,:date)
+    Servresource.create(update)
+    render :text => "200 OK\n"
+  end
 end
